637d3715118a678410b8694f6d0a0edd3a23f95a
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-08-11 17:28:11 +0500

    fix buildTree where tree was built but lost there was no return statement so tree would not be assigned to root

--- src/tree.js
+++ src/tree.js
@@ -3,15 +3,37 @@ export class binaryTree {
   // root of the tree this is the first node
   #root;
   constructor(sortedArray) {
-    this.#root = null;
-    this.#buildTree(sortedArray, 0, sortedArray.length - 1);
+    this.#root = this.#buildTree(
+      sortedArray,
+      0,
+      sortedArray.length - 1,
+      this.#root
+    );
   }
 
-  #buildTree(array, start, end, root = this.#root) {
+  #buildTree(array, start, end, root) {
     if (start > end) return null;
-    let mid = start + (end - start) / 2;
+    let mid = start + Math.floor((end - start) / 2);
     root = new node(array[mid]);
-    this.#buildTree(array, start, mid - 1, root.leftNode);
-    this.#buildTree(array, mid + 1, end, root.rightNode);
+    root.leftNode = this.#buildTree(array, start, mid - 1, root.leftNode);
+    root.rightNode = this.#buildTree(array, mid + 1, end, root.rightNode);
+    return root;
   }
+  prettyPrint = (node = this.#root, prefix = '', isLeft = true) => {
+    if (node === null || node === undefined) {
+      return;
+    }
+
+    this.prettyPrint(
+      node.rightNode,
+      `${prefix}${isLeft ? '│   ' : '    '}`,
+      false
+    );
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.nodeValue}`);
+    this.prettyPrint(
+      node.leftNode,
+      `${prefix}${isLeft ? '    ' : '│   '}`,
+      true
+    );
+  };
 }
