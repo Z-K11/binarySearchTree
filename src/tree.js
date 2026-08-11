99e58f0791f15a5888b41ce2367257d3a2f945bd
theZK11 <zulqarnainbazidkhail@gmail.com>
2026-08-11 19:24:11 +0500

    create method insert which inserts the value into the bst

--- src/tree.js
+++ src/tree.js
@@ -2,6 +2,8 @@ import { node } from './node.js';
 export class binaryTree {
   // root of the tree this is the first node
   #root;
+
+  // constructor builds the tree using an array
   constructor(sortedArray) {
     this.#root = this.#buildTree(
       sortedArray,
@@ -11,6 +13,7 @@ export class binaryTree {
     );
   }
 
+  // recursively builds the binary tree using a provided sorted array
   #buildTree(array, start, end, root) {
     if (start > end) return null;
     let mid = start + Math.floor((end - start) / 2);
@@ -19,6 +22,8 @@ export class binaryTree {
     root.rightNode = this.#buildTree(array, mid + 1, end, root.rightNode);
     return root;
   }
+
+  // prints the binary tree recursively
   prettyPrint = (node = this.#root, prefix = '', isLeft = true) => {
     if (node === null || node === undefined) {
       return;
@@ -36,6 +41,8 @@ export class binaryTree {
       true
     );
   };
+
+  // recursively searches for a value inside the tree if the value exists returns true
   includes(value) {
     let currentNode = this.#root;
     while (currentNode !== null) {
@@ -44,5 +51,26 @@ export class binaryTree {
       if (currentNode.nodeValue < value) currentNode = currentNode.rightNode;
     }
     return false;
+  }
+  insert(value) {
+    if (this.#root === null) {
+      this.#root = new node(value);
+      return;
+    } else this.traverse(value, this.#root);
+  }
+  traverse(value, currentNode) {
+    if (currentNode.nodeValue < value) {
+      if (currentNode.rightNode === null) {
+        currentNode.rightNode = new node(value);
+        return;
+      }
+      this.traverse(value, currentNode.rightNode);
+    } else if (currentNode.nodeValue > value) {
+      if (currentNode.leftNode === null) {
+        currentNode.leftNode = new node(value);
+        return;
+      }
+      this.traverse(value, currentNode.leftNode);
+    }
   }
 }
